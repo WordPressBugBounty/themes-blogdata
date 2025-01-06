e3f4b4280688f1324fec0b35b7a65e324ff12a7b
Sean Murphy <sean@iamseanmurphy.com>
2025-01-06 03:19:27 +0000

    1.2.1

--- blogdata/readme.txt
+++ blogdata/readme.txt
@@ -4,8 +4,8 @@ Author: Themeansar
 Requires at least: WordPress 5.4
 Tested up to: WordPress 6.7
 Requires PHP: 7.4
-Stable tag: 1.2.0
-Version: 1.2.0
+Stable tag: 1.2.1
+Version: 1.2.1
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 Tags: one-column, two-columns ,right-sidebar, flexible-header, custom-background, custom-header, custom-menu, editor-style, featured-images, footer-widgets,  theme-options, threaded-comments, rtl-language-support, translation-ready, full-width-template, custom-logo, blog, news
@@ -157,3 +157,6 @@ License: CC0 Public Domain
 
 = Version 1.2.0
 * Added Search Page Function.
+
+= Version 1.2.1
+* Fixed Full Width Page Issue.
--- blogdata/templates/fullwidth.php
+++ blogdata/templates/fullwidth.php
@@ -12,17 +12,19 @@
 get_header(); ?>
 <main id="content" class="fullwidth-class content">
   <div class="container">
+    <!--==================== breadcrumb section ====================-->
+    <?php do_action('blogdata_breadcrumb_content'); ?>
     <div class="row">
-      <!--==================== breadcrumb section ====================-->
-      <?php do_action('blogdata_breadcrumb_content'); ?>
-      <div class="col-lg-12 bs-card-box padding-20">
-        <?php while ( have_posts() ) : the_post();
-            the_content(); 
-            // If comments are open or we have at least one comment, load up the comment template.
-            if ( comments_open() || get_comments_number() ) :
-              comments_template();
+      <div class="col-lg-12">
+        <div class="bs-card-box wd-back">
+          <?php while ( have_posts() ) : the_post();
+          the_content(); 
+          // If comments are open or we have at least one comment, load up the comment template.
+          if ( comments_open() || get_comments_number() ) :
+            comments_template();
           endif;
-        endwhile; // End of the loop. ?>
+          endwhile; // End of the loop. ?>
+        </div>
       </div>
     </div>
   </div>
